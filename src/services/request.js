5d45fd5d1dbc96fd689201f5355176892cda9355
emily baier <emilyrbaier@gmail.com>
2019-06-10 17:57:49 -0700

    i think its broken but i'm really not sure

---
+++ src/services/request.js
@@ -0,0 +1,17 @@
+const request = (path, method, body) => {
+  return fetch(`${process.env.API_URL}${path}`, {
+    method,
+    headers: { 'Content-Type': 'application/json' },
+    body: body ? JSON.stringify(body) : null
+  })
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw `unable to ${path}`;
+      return json;
+    });
+};
+
+export const post = (path, body) => request(path, 'POST', body);
+export const get = path => request(path, 'GET');
+export const put = (path, body) => request(path, 'PUT', body);
+export const del = path => request(path, 'DELETE');
