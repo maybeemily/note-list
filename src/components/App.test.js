bef10bd7bfb13f4312ff5c46850b4cce98bdbbdb
emily baier <emilyrbaier@gmail.com>
2019-06-11 16:29:36 -0700

    created a jest mock to get snapshot test to pass

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+jest.mock('../services/auth.js');
+
 describe('App component', () => {
   it('renders App', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 });
-  
