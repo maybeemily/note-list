d522397c09b493e9e3d612530233ee4872bbbb69
emily baier <emilyrbaier@gmail.com>
2019-06-11 15:29:50 -0700

    Set up Home, App, and Callback js

---
+++ src/containers/auth/Callback.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import React, { PureComponent } from 'react';
+
+export class Callback extends PureComponent {
+  render() {
+    return <h1>callback</h1>;
+  }
+}
